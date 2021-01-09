8797a77646a5a4cc11bca4446db9defa1b7023c2
Viktoriya <vfitkovskaya@gmail.com>
2021-01-09 16:01:17 +0200

    added HW-12

---
+++ app.js
@@ -0,0 +1,30 @@
+//task 1
+
+/*let res=prompt("Print your number");
+let numPow=Math.pow(res, 2);
+alert(numPow);*/
+
+
+//task 2
+
+/*let userName=prompt('Ім`я:','Без імені');
+let depositAmount=prompt("Сума депозиту:", 5000);
+let term=prompt('Кількість місяців дії депозиту:', 1);
+let annual=20;
+let interest=(depositAmount*(annual/100)*term)/12;
+alert('Шановний(а) '+userName +', Ви внесли '+depositAmount+' грн., під '+annual+'% річних, на термін '
+    +term +' місяці(в). За цей період часу Ви заробите '+interest+' грн. ');*/
+
+
+//task 3
+
+let a=Number(prompt("print the first number:"));
+let b=Number(prompt('Print the second number:'));
+let addition=a+b;
+let subtraction=a-b;
+let multiplication=a*b;
+let division=a/b;
+
+alert(a+' + '+b+' = '+addition+'\n'+a+' - '+b+' = '+subtraction
+    +'\n'+a+' * '+b+' = '+multiplication+'\n'+a+' / '+b+' = '+division);
+
